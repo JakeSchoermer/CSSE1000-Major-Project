1c9675fc5f8344ff000eded7adb47a32c5f78b87
Jake Schoermer <jake@secretservers.net>
2011-10-20 14:55:18 +1000

    Added Stub for Reset Btn

--- src/reset_btn.c
+++ src/reset_btn.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
+/* Inititalise PMOD on JH
+**		- Reset Button on Pin7 on DDRD (Maps to Button 0 on PMOD)
+*/
+
 void init_reset_btn(void) {
+	
+	DDRD |= (1 << 7);
 
 }
