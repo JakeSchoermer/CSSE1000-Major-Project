d7246b18e047ee5cda48a0b0f80d29ca07af9a64
Jake Schoermer <jake@secretservers.net>
2011-10-25 12:12:56 +1000

    added project.h to vcs

---
+++ src/project.h
@@ -0,0 +1,11 @@
+#ifndef _CSSE1000_MAIN
+
+extern uint16_t high_score;
+
+extern uint8_t show_high_score;
+
+extern uint8_t seven_seg_data[];
+
+extern uint8_t seven_seg_cat;
+
+#endif /*_CSSE1000_MAIN*/
