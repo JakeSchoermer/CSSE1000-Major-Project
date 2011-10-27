b2d02c38038bfa9ea826633392314e8f004a54c5
Jake Schoermer <jake@secretservers.net>
2011-10-27 14:35:49 +1000

    update

--- src/pmod.c
+++ src/pmod.c
@@ -29,23 +29,23 @@ void init_pmod(void) {
 void outputHealth(int health) {
 	switch (health) {
 		case 0:
-			PORTE &= 0x0F;
+			PORTE = 0x0F;
 			break;
 		
 		case 1:
-			PORTE &= 0x1F;
+			PORTE = 0x1F;
 			break;
 			
 		case 2:
-			PORTE &= 0x2F;
+			PORTE = 0x2F;
 			break;
 			
 		case 3:
-			PORTE &= 0x4F;
+			PORTE = 0x4F;
 			break;
 			
 		case 4:
-			PORTE &= 0x8F;
+			PORTE = 0xFF;
 			break;
 	}
 }
